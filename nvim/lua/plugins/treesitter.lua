3eda03a4afcb3afb1252b6b49369905a2243ddaa
Roman Volkov <email@romanvolkov.ru>
2023-11-28 12:32:07 +0100

    some tweaks for nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -53,4 +53,10 @@ return {
       },
     },
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "LazyFile",
+    enabled = true,
+    opts = { mode = "cursor", max_lines = 3 },
+  },
 }
