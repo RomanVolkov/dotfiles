b3eada6bb971790ac1c2a20e386e81658c61a799
Roman Volkov <email@romanvolkov.ru>
2025-05-13 11:56:35 +0200

    x_threads; increase timeout to show tmux panes

--- nvim/lua/plugins/x_thread.lua
+++ nvim/lua/plugins/x_thread.lua
@@ -1,16 +1,10 @@
 return {
   "https://github.com/RomanVolkov/x_threads.nvim",
   -- dir = "/Users/romanvolkov/dev/personal/twitter_thread",
-  opts = {},
+  opts = {
+    -- Optional: Customize max tweet length (default: 195)
+    max_tweet_length = 190,
+    -- Optional: Customize counter format (default: "[%d/%d] ")
+    counter_format = "[%d/%d] ",
+  },
 }
-
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
--- some text -- some text -- some text -- some text -- some text
