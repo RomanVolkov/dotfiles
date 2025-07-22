04cf2724be0ccf969d190521a1711892caa73823
Roman Volkov <email@romanvolkov.ru>
2025-07-22 21:43:31 +0200

    remoge go_get plugin; change tmux shortcuts

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -11,7 +11,9 @@ return {
     providers = {
       openai = {
         endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        -- model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        model = "gpt-4.1-mini",
+        -- disable_tools = true,
         extra_request_body = {
           -- timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
           temperature = 0.75,
@@ -25,7 +27,6 @@ return {
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
@@ -36,7 +37,7 @@ return {
     "stevearc/dressing.nvim", -- for input provider dressing
     "folke/snacks.nvim", -- for input provider snacks
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -58,7 +59,7 @@ return {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
-        file_types = { "Avante" },
+        file_types = { "markdown", "Avante" },
       },
       ft = { "markdown", "Avante" },
     },
