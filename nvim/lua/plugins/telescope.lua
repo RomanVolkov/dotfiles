35abc7f4ba56eddcdb85ad3ce83a58cb98a21b0e
Roman Volkov <email@romanvolkov.ru>
2025-12-01 22:16:01 +0100

    change telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,13 +2,13 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = function()
     return {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Search [2F]iles" },
-      { "<leader>gg", "<cmd>Telescope live_grep<cr>", desc = "Search [2G]rep" },
-      { "<leader>dd", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Search [2D]ocument Symbols" },
-      { "<leader>oo", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Search Symbols [2O]" },
+      { "<leader><C-f>", "<cmd>Telescope find_files<cr>", desc = "Search Files" },
+      { "<leader><C-g>", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+      { "<leader><C-d>", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Search Document Symbols" },
+      { "<leader><C-o>", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Search Workspace Symbols" },
       { "<leader>sw", "<cmd>Telescope diagnostics<cr>", desc = "[S]earch Diagnostics/[W]arnings" },
       { "<leader>se", vim.diagnostic.setloclist, desc = "[S]earch Diagnostics/[E]rrors" },
-      { "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Search [2B]uffers" },
+      { "<leader><C-b>", "<cmd>Telescope buffers<cr>", desc = "Search Buffers" },
     }
   end,
 }
