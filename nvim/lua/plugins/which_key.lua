7905c4e5cf1fff859695a44964cb64cd91b66c1f
Roman Volkov <roman.volkov.code@gmail.com>
2026-05-21 10:58:08 +0200

    (fix): declare which-key groups

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -3,5 +3,18 @@ return {
   event = "VeryLazy",
   opts = {
     delay = 300,
+    spec = {
+      { "<leader>s", group = "search/symbols" },
+      { "<leader>b", group = "buffers" },
+      { "<leader>f", group = "files" },
+      { "<leader>x", group = "lists/trouble" },
+      { "<leader>c", group = "code" },
+      { "<leader>w", group = "windows" },
+      { "<leader>g", group = "git" },
+      { "<leader>q", group = "quit/sessions" },
+      { "<leader>d", group = "debug" },
+      { "<leader>r", group = "run/overseer" },
+      { "<leader>u", group = "UI" },
+    },
   },
 }
